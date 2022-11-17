52e71c55dbbd5ab6bc3250085e44a14862e01ea6
Amila Madusanka <amilacreativelab@gmail.com>
2022-11-18 00:49:05 +0530

    adding aws backend

--- src/services/common/config.js
+++ src/services/common/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://localhost:8080/api/`, //'http://ec2-54-212-192-107.us-west-2.compute.amazonaws.com:8080/api', //YOUR_API_URL HERE
+  baseURL: `http://ec2-35-90-155-53.us-west-2.compute.amazonaws.com:8080/api/`, //'http://ec2-54-212-192-107.us-west-2.compute.amazonaws.com:8080/api', //YOUR_API_URL HERE
   headers: {
     'Content-Type': 'application/json',
   },
